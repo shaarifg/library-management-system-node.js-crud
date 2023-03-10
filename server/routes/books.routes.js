8f499b93d8bc640daa22e10fb68762b74c09fd66
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-10 13:57:47 +0530

    added auth middleware

--- server/routes/books.routes.js
+++ server/routes/books.routes.js
@@ -6,9 +6,10 @@ import {
   getBookById,
   updateBook,
 } from "../controllers/books.controller.js";
+import { verifyUser } from "../middlewares/auth.js";
 const router = express.Router();
 
-router.post("/books", addBook);
+router.post("/books", verifyUser, addBook);
 router.get("/books/:id", getBookById);
 router.get("/books", getAllBooks);
 router.put("/books/:id", updateBook);
