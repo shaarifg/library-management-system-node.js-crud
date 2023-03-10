bf240f429fb531d6d7fe2cd602c649540620784d
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-10 17:20:24 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -2,13 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/books.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import cors from "cors";
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const dbURL =
   "mongodb+srv://sharif:books@inittial.esx4vlw.mongodb.net/allbooks?retryWrites=true&w=majority";
-
 
 mongoose
   .connect(dbURL, {
