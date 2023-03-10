f15ec345d2e36427e9e8bcd6d6f4bd4c3c28844b
Mohammad Sharif <ermohdsharif@gmail.com>
2023-03-10 13:56:28 +0530

    added more fields

--- server/models/books.entity.js
+++ server/models/books.entity.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const booksSchema = new mongoose.Schema({
   name: String,
-  author:String,
-  genre:String,
-  coverURL:String,
-  price:Number
+  author: String,
+  genre: String,
+  coverURL: String,
+  price: Number,
 });
 
-export default mongoose.model('books', booksSchema)
+export default mongoose.model("books", booksSchema);
